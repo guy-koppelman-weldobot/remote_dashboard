652ae6704e56041de697579c02bdc2d39d0aae56
Weldobot1@outlook.com <Weldobot1@outlook.com>
2020-12-20 11:52:12 +0200

    2 servers

---
+++ my-app/src/components/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <p>404</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.container{
+  display: flex;
+  flex-direction:column;
+  justify-content: space-around;
+  align-items: center;
+}
+ P {
+    font-size: 60px;
+  }
+</style>
